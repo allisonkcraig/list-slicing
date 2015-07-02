89a62b42204ef7ae67936f512122cff5690da27c
Hackbright Student <info@hackbrightacademy.com>
2015-07-02 16:32:17 -0700

    first commit

---
+++ examples.py
@@ -0,0 +1,13 @@
+string_list = []
+int_list = []
+
+string_list.append("I am a string!")
+int_list.append(range(6))
+string_list.append(["One", "Two"])
+int_list.append(5-8)
+print string_list
+print int_list
+
+print string_list[1]
+
+print string_list[1][1]
